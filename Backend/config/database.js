b3a76719bd4d3d23b67837cabc1ba2534bf9b2a1
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-04-27 23:28:30 +0530

    done installation

---
+++ Backend/config/database.js
@@ -0,0 +1,14 @@
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+const connectDB = async () => {
+   try{
+        await mongoose.connect(process.env.DATABASE_URI)
+        console.log("Database connection successfully")
+   }catch(error) {
+        console.log(error.message)
+        console.log("Failed to connect with Database")
+   }
+}
+
+module.exports = connectDB;
