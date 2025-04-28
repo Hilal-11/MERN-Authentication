d8ae4393865b370e04957173bf63a31ee81be6e4
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-04-29 01:26:26 +0530

    Initilize the frontend and basic setup

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1 className='text-center text-5xl font-extrabold py-10'>MERN Authentication and Autherization</h1>
+      <h1 className='text-center text-5xl poppins-extrabold py-10'>MERN Authentication and Autherization</h1>
     </div>
   )
 }
